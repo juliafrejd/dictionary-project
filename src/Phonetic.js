5fda7d5a2f9ba8d8894b50de8eedec9e2cc572c5
juliafrejd <118212457+juliafrejd@users.noreply.github.com>
2023-09-24 10:59:50 +0100

    Synonyms and phonetic

--- src/Phonetic.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Phonetic(props) {
-  console.log(props.phonetic);
-  return "Hello from Phonetic";
-}
--- src/Results.js
+++ src/Results.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./Results.css";
 import Meaning from "./Meaning";
-import Phonetic from "./Phonetic";
 
 export default function Results(props) {
+  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.phonetic.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
+        <p>/{props.results.phonetic}/</p>
 
         {props.results.meanings.map(function (meaning, index) {
           return (
